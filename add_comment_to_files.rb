b204864928c438aeed61e1ec55dc59fcada41818
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-11-10 23:57:02 +0000

    Added path auto-comment generator thing - flashy

---
+++ add_comment_to_files.rb
@@ -0,0 +1,31 @@
+def add_comment_to_file(file_path)
+  comment_char, comment_end = comment_chars_for_file(file_path)
+  comment = "#{comment_char}#{file_path}#{comment_end}"
+  content = File.read(file_path)
+  content = "#{comment}\n#{content}"
+  File.write(file_path, content)
+end
+
+def comment_chars_for_file(file_path)
+  case File.extname(file_path)
+  when '.rb'
+    ['#', '']
+  when '.js'
+    ['//', '']
+  when '.css'
+    ['/* ', '']
+  when '.html'
+    ['<!--', '-->']
+  when '.erb'
+    ['<!--', '-->']
+  end
+end
+
+def process_files(directory, extensions)
+  Dir.glob(File.join(directory, "**/*.{#{extensions.join(',')}}")).each do |file|
+    add_comment_to_file(file)
+  end
+end
+directory_to_search = '/workspaces/Inventory-Management-System/'
+file_extensions = %w[rb html html.erb css js]
+process_files(directory_to_search, file_extensions)
---
+++ remove_generated_comments.rb
@@ -0,0 +1,27 @@
+# remove_generated_comments.rb
+
+def remove_generated_comments(file_path)
+  # Read the existing content of the file
+  content = File.read(file_path)
+
+  # Identify and remove lines matching the generated comments
+  content = content.gsub(/^(\s*#|#\{|\/\/|\/\*|\<!--).*#{Regexp.escape(file_path)}.*?\n/m, '')
+
+  # Write the modified content back to the file
+  File.write(file_path, content)
+end
+
+def process_files(directory, extensions)
+  Dir.glob(File.join(directory, "**/*.{#{extensions.join(',')}}")).each do |file|
+    remove_generated_comments(file)
+  end
+end
+
+# Specify the directory to start the search
+directory_to_search = '/workspaces/Inventory-Management-System/'
+
+# Specify the file extensions to target
+file_extensions = %w[rb html html.erb css js]
+
+# Process the files in the specified directory with the given extensions
+process_files(directory_to_search, file_extensions)
