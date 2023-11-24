626e142060388869cda7126f95718fa097b40f07
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-11-24 18:44:16 +0000

    tiny updates to HTML stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 #/workspaces/Inventory-Management-System/config/routes.rb
 Rails.application.routes.draw do
-  root "orders#index"
+  root "products#index"
   get '/forgot_password', to: 'users#forgot_password'
   devise_for :users, :controllers => { :registrations => "custom_devise/registrations" }
   resources :users
